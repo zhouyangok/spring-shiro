6e598127a7890cf9dab26d7101c273c7cf5d578a
wangzhixuan <wangzhixuan@iqianjin.com>
2015-08-19 17:46:40 +0800

    update

---
+++ src/main/java/com/wangzhixuan/service/OrganizationService.java
@@ -0,0 +1,11 @@
+package com.wangzhixuan.service;
+
+import java.util.List;
+
+import com.wangzhixuan.vo.Tree;
+
+public interface OrganizationService {
+
+    List<Tree> findTree();
+
+}
---
+++ src/main/java/com/wangzhixuan/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,17 @@
+package com.wangzhixuan.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.wangzhixuan.service.OrganizationService;
+import com.wangzhixuan.vo.Tree;
+@Service
+public class OrganizationServiceImpl implements OrganizationService {
+
+    @Override
+    public List<Tree> findTree() {
+        return null;
+    }
+
+}
