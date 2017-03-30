a436b11f3e931f50c03e4e3682899bfba96850e6
如梦技术 <596392912@qq.com>
2017-03-30 11:35:09 +0800

    fixed shiro ByteSource redis 序列化反序列化问题

--- src/main/java/com/wangzhixuan/commons/shiro/ShiroByteSource.java
+++ src/main/java/com/wangzhixuan/commons/shiro/ShiroByteSource.java
@@ -1,11 +1,12 @@
 package com.wangzhixuan.commons.shiro;
 
-import java.io.File;
-import java.io.InputStream;
 import java.io.Serializable;
+import java.util.Arrays;
 
+import org.apache.shiro.codec.Base64;
+import org.apache.shiro.codec.CodecSupport;
+import org.apache.shiro.codec.Hex;
 import org.apache.shiro.util.ByteSource;
-import org.apache.shiro.util.SimpleByteSource;
 
 /**
  * shiro密码盐加密所用
@@ -13,33 +14,73 @@ import org.apache.shiro.util.SimpleByteSource;
  * @author L.cm
  *
  */
-public class ShiroByteSource extends SimpleByteSource implements Serializable {
+public class ShiroByteSource implements ByteSource, Serializable {
 	private static final long serialVersionUID = -6814382603612799610L;
+	private volatile byte[] bytes;
+	private String cachedHex;
+	private String cachedBase64;
 
-	public ShiroByteSource(byte[] bytes) {
-		super(bytes);
-	}
-
-	public ShiroByteSource(ByteSource source) {
-		super(source);
-	}
-
-	public ShiroByteSource(char[] chars) {
-		super(chars);
-	}
-
-	public ShiroByteSource(File file) {
-		super(file);
-	}
-
-	public ShiroByteSource(InputStream stream) {
-		super(stream);
-	}
+	public ShiroByteSource() {}
 
 	public ShiroByteSource(String string) {
-		super(string);
+		this.bytes = CodecSupport.toBytes(string);
+	}
+	
+	public void setBytes(byte[] bytes) {
+		this.bytes = bytes;
 	}
 
+	@Override
+	public byte[] getBytes() {
+		return this.bytes;
+	}
+
+	@Override
+	public String toHex() {
+		if ( this.cachedHex == null ) {
+			this.cachedHex = Hex.encodeToString(getBytes());
+		}
+		return this.cachedHex;
+	}
+
+	@Override
+	public String toBase64() {
+		if ( this.cachedBase64 == null ) {
+			this.cachedBase64 = Base64.encodeToString(getBytes());
+		}
+		return this.cachedBase64;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.bytes == null || this.bytes.length == 0;
+	}
+	
+	@Override
+	public String toString() {
+		return toBase64();
+	}
+
+	@Override
+	public int hashCode() {
+		if (this.bytes == null || this.bytes.length == 0) {
+			return 0;
+		}
+		return Arrays.hashCode(this.bytes);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+		if (o instanceof ByteSource) {
+			ByteSource bs = (ByteSource) o;
+			return Arrays.equals(getBytes(), bs.getBytes());
+		}
+		return false;
+	}
+	
 	/**
 	 * Returns a new {@code ByteSource} instance representing the specified string's bytes.  The byte
 	 * array is obtained assuming {@code UTF-8} encoding.
