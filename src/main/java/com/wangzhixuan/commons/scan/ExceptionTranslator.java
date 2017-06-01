3b1a93cc2168b5a97a8bad0b8aec47fe5b657fa6
如梦技术 <596392912@qq.com>
2017-06-01 09:41:38 +0800

    普通的异常交给 {@link ExceptionResolver} 处理

--- src/main/java/com/wangzhixuan/commons/scan/ExceptionTranslator.java
+++ src/main/java/com/wangzhixuan/commons/scan/ExceptionTranslator.java
@@ -59,12 +59,16 @@ public class ExceptionTranslator {
 		return _result;
 	}
 	
-	@ExceptionHandler(Exception.class)
-	@ResponseBody
-	public Result processException(Exception ex) {
-		log.error(ex.getMessage(), ex);
-		Result result = new Result();
-		result.setMsg(ex.getMessage());
-		return result;
-	}
+	/**
+	 * 普通的异常交给 {@link ExceptionResolver} 处理
+	 * 兼容 页面 异常和ajax异常
+	 */
+//	@ExceptionHandler(Exception.class)
+//	@ResponseBody
+//	public Result processException(Exception ex) {
+//		log.error(ex.getMessage(), ex);
+//		Result result = new Result();
+//		result.setMsg(ex.getMessage());
+//		return result;
+//	}
 }
